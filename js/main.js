1f5c286c2078a574d1e07e357144cb8f1911c6a7
hfc123 <hongfuchang1234@gmail.com>
2018-05-21 20:14:45 +0800

    5/21/20/14
    5/21/20/14

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
 	var mySwiper = new Swiper ('.swiper-container', {
     autoplay: true,//可选选项，自动滑动
     delay:2000,
@@ -17,5 +17,48 @@ $(function(){
     prevButton: '.swiper-button-prev',
     // 如果需要滚动条
     scrollbar: '.swiper-scrollbar'
-  })  
+  });
+  //热卖商品热评商品的show hide操作
+  console.log('！11122')
+  $(".firstline li").eq(0).find("i").show();
+  $(".firstline li").eq(0).addClass("liclass");
+  $(".firstline li").eq(0).hover(function(){
+  	$(".firstline li").eq(1).removeClass("liclass");
+  	$(".firstline li").eq(1).find("i").hide();
+  	$(".firstline li").eq(2).removeClass("liclass");
+  	$(".firstline li").eq(2).find("i").hide();
+  	$(this).addClass("liclass");
+  	$(this).find("i").show();
+  	$(".remai").css("display",'block');
+  	$(".reping").css("display",'none');
+  	$(".newshop").css("display",'none');
+  },function(){
+  })
+  $(".firstline li").eq(1).hover(function(){
+  	$(this).addClass("liclass");
+  	$(this).find("i").addClass("iclass");
+  	$(".firstline li").eq(0).removeClass("liclass");
+  	$(".firstline li").eq(0).find("i").hide();
+  	$(".firstline li").eq(2).removeClass("liclass");
+  	$(".firstline li").eq(2).find("i").hide();
+  	$(this).addClass(".liclass");
+  	$(this).find("i").show();
+  	$(".remai").css("display",'none');
+  	$(".reping").css("display",'block');
+  	$(".newshop").css("display",'none');
+  },function(){
+  })
+  $(".firstline li").eq(2).hover(function(){
+  	$(this).addClass("liclass");
+  	$(this).find("i").show();
+  	$(".firstline li").eq(0).removeClass("liclass");
+  	$(".firstline li").eq(0).find("i").hide();
+  	$(".firstline li").eq(1).removeClass("liclass");
+  	$(".firstline li").eq(1).find("i").hide();
+  	$(".remai").css("display",'none');
+  	$(".reping").css("display",'none');
+  	$(".newshop").css("display",'block');
+  },function(){
+  })
+  
 });
